1c5aea90ed7e0cfb4201fc2007665ea257fc5412
dashakarcevskaya <darakarcevskaa@gmail.com>
2019-11-19 14:07:12 +0300

    fixed GettransferTest.java

--- WebDriver/src/test/java/by/bsu/gettransfer/test/GettransferTest.java
+++ WebDriver/src/test/java/by/bsu/gettransfer/test/GettransferTest.java
@@ -11,11 +11,11 @@ import org.testng.annotations.Test;
 public class GettransferTest {
     private WebDriver driver;
     private HomePage page;
-    private final String pickUpLocation = "Minsk, Belarus";
-    private final String returnLocation = "Grodno, Belarus";
-    private final String phoneNumber = "123456";
-    private final String fullName = "Darya";
-    private final String emptyPhoneNumber = "";
+    private final String PICK_UP_LOCATION = "Minsk, Belarus";
+    private final String RETURN_LOCATION = "Grodno, Belarus";
+    private final String PHONE_NUMBER = "123456";
+    private final String FULL_NAME = "Darya";
+    private final String EMPTY_PHONE_NUMBER = "";
 
     @BeforeClass
     public void browserSetUp() {
@@ -28,18 +28,18 @@ public class GettransferTest {
     @Test
     public void attemptSignUp() {
         page.signUp();
-        page.enterFullName(fullName);
-        page.enterPhoneNumberInSignUpForm(emptyPhoneNumber);
+        page.enterFullName(FULL_NAME);
+        page.enterPhoneNumberInSignUpForm(EMPTY_PHONE_NUMBER);
         page.signUpClick();
         Assert.assertTrue(page.isErrorMessageVisible1());
     }
 
     @Test
     public void bookingShowsErrorWhenInvalidNumber() {
-        page.inputPickUpLocation(pickUpLocation);
-        page.inputReturnLocation(returnLocation);
+        page.inputPickUpLocation(PICK_UP_LOCATION);
+        page.inputReturnLocation(RETURN_LOCATION);
         page.selectCar();
-        page.enterPhoneNumber(phoneNumber);
+        page.enterPhoneNumber(PHONE_NUMBER);
         page.getOffers();
         Assert.assertTrue(page.isErrorMessageVisible2());
 
